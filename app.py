66aa1217f20187fcca8774aa210cc754d90e77c8
Lucasivan7 <lucasivancaballero@hotmail.com>
2022-10-26 01:59:39 -0300

    First commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def login():
+    return render_template("login.html")
+
+@app.route("/iniciar")
+def inicio():
+    return render_template("iniciar.html")
+
+@app.route("/quiz1")
+def quiz1():
+    return render_template("quiz1.html")
